6e43c070bc9e113e3271b05d860449ac2ae4df53
pabloc33 <pablo.contino33@gmail.com>
2022-11-20 18:44:10 -0300

    Primer commit

---
+++ src/stories/components/MyLabel.stories.tsx
@@ -0,0 +1,49 @@
+import { MyLabel } from "../../components/MyLabel";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+export default {
+  title: "UI/MyLabel",
+  component: MyLabel,
+  argTypes: {
+    size: { control: "select" },
+    color: { control: "select" },
+  },
+} as ComponentMeta<typeof MyLabel>;
+
+const Template: ComponentStory<typeof MyLabel> = (args) => (
+  <MyLabel {...args} />
+);
+
+export const Basic = Template.bind({});
+Basic.args = {
+  size: "normal",
+  allCaps: false, //capitaliza toda la palabra
+};
+
+export const AllCaps = Template.bind({});
+AllCaps.args = {
+  size: "normal",
+  allCaps: true,
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  size: "normal",
+  color: "secondary",
+};
+
+export const Tertiary = Template.bind({});
+Tertiary.args = {
+  size: "normal",
+  color: "tertiary",
+};
+
+export const CustomFontColor = Template.bind({});
+CustomFontColor.args = {
+  fontColor: "#5517ac",
+  size: "h1",
+};
+
+// CustomFontColor
+// fontColor: #5517ac
+// size: h1
